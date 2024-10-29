59079feaaf2ba627a5379d1424e353733547fbd9
Ezra Natanael <ezrantn@proton.me>
2024-10-29 19:46:30 +0700

    add roughly schema

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   async signIn(email: string, pass: string): Promise<{ access_token: string }> {
     // Sign In Logic Here
